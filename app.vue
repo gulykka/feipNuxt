77aac2997b8640b013c5566d99a83bfd29beb628
gulykka <bulygina.ulyana@mail.ru>
2024-03-02 22:46:54 +1000

    initial commit

---
+++ app.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="wrapper">
+    <h1>HELLO WORLD!!!</h1>
+  </div>
+</template>
+<script>
+console.log("Hello World")
+</script>
+<style scoped lang="sass">
+#wrapper
+  display: flex
+  justify-content: center
+  align-items: center
+  width: 100%
+  height: 100%
+  position: absolute
+  h1
+    color: #ffb6dd
+    font-size: 3em
+    text-shadow: 1px 1px black
+  h1::first-letter
+    color: #e672af
+
+
+</style>
