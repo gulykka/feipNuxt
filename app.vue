4cdce834f67ff8608a90cb3f3703d8d6b2475110
gulykka <bulygina.ulyana@mail.ru>
2024-04-06 01:16:09 +1000

    responsive layout

--- app.vue
+++ app.vue
@@ -1,4 +1,6 @@
 <template>
+  <DialogView
+      v-if="isVisible" @visible="showDialog"></DialogView>
   <div class="container">
     <div class="header">
       <div class="information">
@@ -8,17 +10,23 @@
           <a>Новости</a>
           <a>Контакты</a>
         </div>
+        <ToolbarRoot>
+          <ToolbarButton class="burger_menu" v-on:click="showDialog">
+            <img src="public/images/menu.svg">
+          </ToolbarButton>
+        </ToolbarRoot>
       </div>
       <div class="connection">
         <div class="telephone">
           <img src="public/images/telephone.svg">
           <a href=»tel>+7 (900) 900-90-90</a>
         </div>
-        <button>Оставить заявку</button>
+        <ToolbarRoot>
+          <ToolbarButton class="button_default">Оставить заявку</ToolbarButton>
+        </ToolbarRoot>
       </div>
     </div>
     <div class="main_container">
-      <h3>Hello World!!!</h3>
     </div>
     <div class="footer">
       <div class="footer_top">
@@ -32,7 +40,7 @@
           <div class="connection_footer">
             <div class="telephone">
               <img src="public/images/telephone.svg">
-              <a href=»tel>+7 (900) 900-90-90</a>
+              <a href="tel">+7 (900) 900-90-90</a>
             </div>
             <div class="email">
               <img src="public/images/email.svg">
@@ -44,7 +52,9 @@
             </div>
           </div>
         </div>
-        <button>Оставить заявку</button>
+        <ToolbarRoot>
+          <ToolbarButton class="button_default">Оставить заявку</ToolbarButton>
+        </ToolbarRoot>
       </div>
       <div class="footer_bottom">
         <label>© Загдом, 2021</label>
@@ -53,14 +63,30 @@
       </div>
     </div>
   </div>
+
 </template>
 
 <script>
-import {ToolbarButton} from 'radix-vue'
+import {ToolbarButton, ToolbarRoot} from 'radix-vue'
 import {defineComponent} from "vue";
+import DialogView from "/public/components/DialogView.vue";
 
 export default defineComponent({
-  components: {ToolbarButton}
+  components: {
+    ToolbarButton,
+    ToolbarRoot,
+    DialogView
+  },
+  data() {
+    return {
+      isVisible: false
+    }
+  },
+  methods: {
+    showDialog() {
+      this.isVisible = !this.isVisible
+    }
+  }
 })
 
 console.log("Hello World")
@@ -95,32 +121,53 @@ a
     font-family: 'Open Sans'
     color: #666666
     font-size: 16px
-    column-gap: 254px
-
-    @media screen and (max-width: 1300px)
-      column-gap: 20px
-
-    .information
-      display: flex
-      align-items: center
-      justify-content: space-between
-      column-gap: 80px
-
-    .information_links
-      display: flex
-      column-gap: 24px
 
 
 
-    .connection
-      display: flex
-      align-items: center
-      column-gap: 24px
+.information
+  display: flex
+  align-items: center
+  justify-content: space-evenly
+  column-gap: 30px
+  @media screen and ( max-width: 1080px)
+    width: 100%
+    justify-content: space-between
+    padding-left: 30px
+    padding-right: 30px
+
+
+.information_links
+  display: flex
+  column-gap: 24px
+  @media screen and ( max-width: 1080px)
+    display: none
+.burger_menu
+  background: #029F59
+  color: white
+  border-radius: 10px
+  border: 2px #029F59 solid
+  padding: 10px
+  transition: all 1s
+  &:hover
+    background: #254741
+    border: 2px #029F59 solid
+
+  @media screen and ( min-width: 1081px)
+    display: none
+
+.connection
+  display: flex
+  align-items: center
+  column-gap: 24px
+  @media screen and ( max-width: 1080px)
+    display: none
 
 .telephone, .email
   display: flex
   column-gap: 8px
   align-items: center
+  @media screen and (max-width: 1300px)
+    padding-right: 60px
 
 .location
   display: flex
@@ -136,27 +183,40 @@ a
   flex-direction: column
   font-family: 'Open Sans'
   background: #254741
-  height: 246px
+  padding-bottom: 20px
   font-size: 14px
   flex-shrink: 0
   row-gap: 32px
+  @media screen and (max-width: 680px)
+    align-items: center
+
 
 .footer_top
   display: flex
   justify-content: space-evenly
   align-items: flex-start
   padding-top: 37px
+  @media screen and (max-width: 680px)
+    flex-direction: column
+    row-gap: 20px
 
 .footer_bottom
   display: flex
-  color: white
-  opacity: 60%
-  padding-left: 170px
+  color: #acacac
+  padding-left: 200px
   column-gap: 100px
-  @media screen and (max-width: 1300px)
+  @media screen and (max-width: 1080px)
     padding-left: 60px
-
-
+    //opacity: 1
+  @media screen and (max-width: 940px)
+    padding-left: 0
+    justify-content: space-around
+    column-gap: 0
+  @media screen and (max-width: 680px)
+    flex-direction: column
+    row-gap: 20px
+  a
+    color: #acacac
   label
     padding-right: 140px
     @media screen and (max-width: 1300px)
@@ -179,7 +239,7 @@ a
   display: flex
   flex-direction: column
   row-gap: 24px
-  width: 202px
+  width: 205px
 
 .navigation
   display: flex
@@ -188,17 +248,26 @@ a
   padding-top: 12px
   @media screen and (max-width: 1300px)
     padding-right: 60px
+  @media screen and (max-width: 1080px)
+    column-gap: 20px
+  @media screen and (max-width: 940px)
+    flex-direction: column
+    row-gap: 20px
 
-button
+.button_default
   padding: 16px 40px
   background: #029F59
   color: white
   border-radius: 10px
   border: 2px #029F59 solid
-  transition: all 1.5s
+  transition: all 1s
   width: 204px
 
   &:hover
     background: #254741
     border: 2px #029F59 solid
+
+
+
+
 </style>
---
+++ public/components/DialogView.vue
@@ -0,0 +1,120 @@
+<template>
+  <div
+      class="window"
+      @click="$emit('visible')">
+  </div>
+  <div class="dialog">
+    <div class="menu_up">
+      <ToolbarRoot>
+        <ToolbarButton
+            class="close_button"
+            @click="$emit('visible')">
+          <img src="public/images/close_button.svg">
+        </ToolbarButton>
+      </ToolbarRoot>
+      <div class="information_links">
+        <a>Реализованные проекты</a>
+        <a>Новости</a>
+        <a>Контакты</a>
+      </div>
+    </div>
+    <div class="connection">
+      <div class="telephone">
+        <img src="public/images/telephone_white.svg">
+        <a href="tel">+7 (900) 900-90-90</a>
+      </div>
+      <div class="email">
+        <img src="public/images/email_white.svg">
+        <a href="mailto:info@gmail.com">info@gmail.com</a>
+      </div>
+      <div class="location">
+        <img src="public/images/location_white.svg">
+        <a>г. Владивосток ул. Выселковая 49, стр. 3</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {ToolbarButton, ToolbarRoot} from 'radix-vue'
+
+export default {
+  name: "DialogView",
+  components: {
+    ToolbarButton, ToolbarRoot
+  },
+  data() {
+    return {
+      isVisible: true
+    }
+  }
+}
+</script>
+
+<style scoped lang="sass">
+.window
+
+  opacity: 0.5
+  background: black
+  width: 100%
+  left: 0
+  top: 0
+  bottom: 0
+  //height: 100vh
+  position: fixed
+
+a
+  color: white
+.dialog
+  width: 40%
+  position: fixed
+  right: 0
+  bottom: 0
+  top: 0
+  display: flex
+  flex-direction: column
+  justify-content: space-between
+  background: #029F59
+  padding: 20px
+
+.close_button
+  padding: 5px
+  width: 32px
+  background: #4bb586
+  height: 32px
+  border: none
+  border-radius: 5px
+  transition: all 1s
+  &:hover
+    opacity: 0.8
+
+.information_links
+  display: flex
+  flex-direction: column
+  color: white
+  row-gap: 24px
+
+.menu_up
+  display: flex
+  flex-direction: column
+  row-gap: 20px
+
+
+
+.connection
+  display: flex
+  flex-direction: column
+  row-gap: 24px
+
+
+.telephone, .email
+  display: flex
+  column-gap: 8px
+  align-items: center
+
+.location
+  display: flex
+  column-gap: 8px
+  align-items: flex-start
+
+</style>
